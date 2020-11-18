80643cac16ca5930bd8ce69c9f63d4ab2482dba1
Kirill <developerpopov@yandex.ru>
2020-11-18 21:58:12 +0300

    Модуль создания списка и его элементов

---
+++ src/view/main-trip-list-events.js
@@ -0,0 +1,7 @@
+export const createTripListEventsTemplate = () => {
+  return `<ul class="trip-events__list"></ul>`;
+};
+
+export const createTripListEventsItemTemplate = () => {
+  return `<li class="trip-events__item"></li>`;
+};
