6547fb1eb2a700d4fdd408222cbed48ac81d36da
Kirill <developerpopov@yandex.ru>
2020-11-24 18:17:26 +0300

    Изменил создание разметки для вывода итоговой цены путешествия
    Теперь цена складывается из общей стоимости всех точек

--- src/view/header-trip-cost.js
+++ src/view/header-trip-cost.js
@@ -1,5 +1,18 @@
-export const createTripCostTemplate = () => {
+// Получаем итоговую цену путешествия
+const getTripCost = (pointsList) => {
+  let tripCost = 0;
+  for (let point of pointsList) {
+    for (let offer of point.currentOffers) {
+      if (offer.checked) {
+        tripCost += offer.cost;
+      }
+    }
+  }
+  return tripCost;
+};
+
+export const createTripCostTemplate = (serverData) => {
   return `<p class="trip-info__cost">
-            Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+            Total: &euro;&nbsp;<span class="trip-info__cost-value">${getTripCost(serverData)}</span>
           </p>`;
 };
