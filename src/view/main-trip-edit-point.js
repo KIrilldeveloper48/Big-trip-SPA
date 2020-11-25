2582ab0beb81c3ddcabdee658758870d0b629d8d
Kirill <developerpopov@yandex.ru>
2020-11-25 20:47:55 +0300

    Изменил прежний способ генерации разметки на reduce

--- src/view/main-trip-edit-point.js
+++ src/view/main-trip-edit-point.js
@@ -3,23 +3,21 @@ import {getPointCost} from './main-trip-point';
 
 // Генерация разметки для списка с типом путешествия
 export const generateTypesListTemplate = (typesList) => {
-  let typesListTemplate = ``;
-  for (let type of typesList) {
-    typesListTemplate += `<div class="event__type-item">
-                            <input id="event-type-${type.toLowerCase()}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type.toLowerCase()}">
-                            <label class="event__type-label  event__type-label--${type.toLowerCase()}" for="event-type-${type.toLowerCase()}-1">${type}</label>
-                          </div>`;
-  }
-  return typesListTemplate;
+  return typesList.reduce((result, type) => {
+    result += `<div class="event__type-item">
+                  <input id="event-type-${type.toLowerCase()}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type.toLowerCase()}">
+                  <label class="event__type-label  event__type-label--${type.toLowerCase()}" for="event-type-${type.toLowerCase()}-1">${type}</label>
+                </div>`;
+    return result;
+  }, ``);
 };
 
 // Генерация разметки для списка городов
 export const generateCitiesListTemplate = (citiesList) => {
-  let citiesListTemplate = ``;
-  for (let city of citiesList) {
-    citiesListTemplate += `<option value="${city}"></option>`;
-  }
-  return citiesListTemplate;
+  return citiesList.reduce((result, city) => {
+    result += `<option value="${city}"></option>`;
+    return result;
+  }, ``);
 };
 
 // Генерация разметки для списка доступных доп. опций конкретной точки
@@ -28,28 +26,27 @@ export const generateOffersListTemplate = (offersList) => {
     return ``;
   }
 
-  let offersListTemplate = ``;
-  for (let offer of offersList) {
+  let offersListTemplate = offersList.reduce((result, offer) => {
+    let offersListInputTemplate = `<input class="event__offer-checkbox  visually-hidden" 
+                                    id="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}-1" type="checkbox" 
+                                    name="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}">`;
+
     if (offer.checked) {
-      offersListTemplate += `<div class="event__offer-selector">
-                              <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}-1" type="checkbox" name="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}" checked>
-                              <label class="event__offer-label" for="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}-1">
-                                <span class="event__offer-title">${offer.name}</span>
-                                &plus;&euro;&nbsp;
-                                <span class="event__offer-price">${offer.cost}</span>
-                              </label>
-                            </div>`;
-    } else {
-      offersListTemplate += `<div class="event__offer-selector">
-                              <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}-1" type="checkbox" name="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}">
-                              <label class="event__offer-label" for="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}-1">
-                                <span class="event__offer-title">${offer.name}</span>
-                                &plus;&euro;&nbsp;
-                                <span class="event__offer-price">${offer.cost}</span>
-                              </label>
-                            </div>`;
+      offersListInputTemplate = `<input class="event__offer-checkbox  visually-hidden" 
+                                  id="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}-1" type="checkbox" 
+                                  name="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}" checked>`;
     }
-  }
+
+    result += `<div class="event__offer-selector">
+                ${offersListInputTemplate}
+                <label class="event__offer-label" for="event-offer-${offer.name.toLowerCase().replace(` `, `-`)}-1">
+                  <span class="event__offer-title">${offer.name}</span>
+                  &plus;&euro;&nbsp;
+                  <span class="event__offer-price">${offer.cost}</span>
+                </label>
+              </div>`;
+    return result;
+  }, ``);
 
   return `<section class="event__section  event__section--offers">
             <h3 class="event__section-title  event__section-title--offers">Offers</h3>
@@ -65,10 +62,11 @@ export const generateDestinationTemplate = (photos, descr) => {
     return ``;
   }
 
-  let photosList = ``;
-  for (let photo of photos) {
-    photosList += `<img class="event__photo" src="${photo}" alt="Event photo">`;
-  }
+  let photosList = photos.reduce((result, photo) => {
+    result += `<img class="event__photo" src="${photo}" alt="Event photo">`;
+    return result;
+  }, ``);
+
   return `<section class="event__section  event__section--destination">
             <h3 class="event__section-title  event__section-title--destination">Destination</h3>
             <p class="event__destination-description">${descr}</p>
