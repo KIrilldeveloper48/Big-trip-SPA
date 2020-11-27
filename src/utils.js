1512919f81f257e2394b3e2ff57559a00821e684
Kirill <developerpopov@yandex.ru>
2020-11-27 19:01:58 +0300

    Перенёс файл utils и добавил несколько функций для рендера

---
+++ src/utils.js
@@ -0,0 +1,40 @@
+export const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+export const getRandomItem = (items) => {
+  if (items.length === 0) {
+    return [];
+  }
+  return items[getRandomInteger(0, items.length - 1)];
+};
+
+// --- Рендер ---
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`); // 1
+  newElement.innerHTML = template; // 2
+
+  return newElement.firstChild; // 3
+};
+
+
