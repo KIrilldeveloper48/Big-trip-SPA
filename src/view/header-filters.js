a9e8c0f617b813d4635ff177d4cabb82fc12d22e
Kirill <developerpopov@yandex.ru>
2020-11-28 18:56:23 +0300

    Отрефакторил код
    Вынес повторяющиеся выражения из генерации разметки в отдельные переменные. Перенёс экспорт в конец документа

--- src/view/header-filters.js
+++ src/view/header-filters.js
@@ -4,10 +4,14 @@ import {createElement} from "../utils";
 const generateFiltersListTemplate = (filtersList) => {
 
   return filtersList.reduce((result, filter) => {
+
+    const filterLowerCase = filter.toLowerCase();
+
     result += `<div class="trip-filters__filter">
-                      <input id="filter-${filter.toLowerCase()}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.toLowerCase()}">
-                      <label class="trip-filters__filter-label" for="filter-${filter.toLowerCase()}">${filter}</label>
-                    </div>`;
+                <input id="filter-${filterLowerCase}" class="trip-filters__filter-input  visually-hidden" 
+                type="radio" name="trip-filter" value="${filterLowerCase}">
+                <label class="trip-filters__filter-label" for="filter-${filterLowerCase}">${filter}</label>
+              </div>`;
     return result;
   }, ``);
 
@@ -16,16 +20,14 @@ const generateFiltersListTemplate = (filtersList) => {
 const createFiltersTemplate = (serverData) => {
   return `<form class="trip-filters" action="#" method="get">
             ${generateFiltersListTemplate(serverData)}
-
             <button class="visually-hidden" type="submit">Accept filter</button>
           </form>`;
 };
 
-export default class Filters {
+class Filters {
   constructor(data) {
     this._element = null;
     this._data = data;
-    this._header = `<h2 class="visually-hidden">Filter events</h2>`;
   }
 
   getTemplate() {
@@ -39,11 +41,9 @@ export default class Filters {
     return this._element;
   }
 
-  getHeader() {
-    return createElement(this._header);
-  }
-
   removeElement() {
     this._element = null;
   }
 }
+
+export default Filters;
