00692d8bd120079255cba6bbd921d687044a6cae
Kirill <developerpopov@yandex.ru>
2020-11-28 18:59:42 +0300

    Отрефакторил код
    Вынес переменную maxCityVisible в const. Упростил условие в функциях getTitleInfo и getDateInfo.  Перенёс экспорт в конец документа

--- src/view/header-info.js
+++ src/view/header-info.js
@@ -1,33 +1,29 @@
-import dayjs from 'dayjs';
-import {createElement} from '../utils';
+import {createElement, getFormatedDate} from '../utils';
+import {maxCityVisible} from '../const';
 
 // Получаем маршрут путешествия
 const getTitleInfo = (pointsList) => {
-  const maxCityVisible = 3;
 
   if (pointsList.length > maxCityVisible) {
     return `${pointsList[0].currentCity} &mdash; ... &mdash; ${pointsList[pointsList.length - 1].currentCity}`;
   }
 
   return pointsList.reduce((result, point) => {
-    if (point !== pointsList[pointsList.length - 1]) {
-      result += `${point.currentCity} &mdash;`;
-    } else {
-      result += `${point.currentCity}`;
-    }
+    const isDash = point !== pointsList[pointsList.length - 1] ? `&mdash; ` : ``;
+
+    result += `${point.currentCity} ${isDash}`;
+
     return result;
   }, ``);
 };
 
 // Получаем дату начала и окончания путешествия
 const getDateInfo = (pointsList) => {
-  const startTripDateMounth = dayjs(pointsList[0].startDate).format(`M`);
-  const EndTripDateMounth = dayjs(pointsList[pointsList.length - 1].endDate).format(`M`);
+  const startDate = pointsList[0].startDate;
+  const endDate = pointsList[pointsList.length - 1].endDate;
+  const choiceFormatDate = getFormatedDate(startDate, `M`) !== getFormatedDate(endDate, `M`) ? getFormatedDate(endDate, `MMM D`) : getFormatedDate(endDate, `D`);
 
-  if (startTripDateMounth !== EndTripDateMounth) {
-    return `${dayjs(pointsList[0].startDate).format(`MMM D`)}&nbsp;&mdash;&nbsp;${dayjs(pointsList[pointsList.length - 1].endDate).format(`MMM D`)}`;
-  }
-  return `${dayjs(pointsList[0].startDate).format(`MMM D`)}&nbsp;&mdash;&nbsp;${dayjs(pointsList[pointsList.length - 1].endDate).format(`D`)}`;
+  return `${getFormatedDate(startDate, `MMM D`)}&nbsp;&mdash;&nbsp;${choiceFormatDate}`;
 };
 
 const createInfoTemplate = (serverData) => {
@@ -41,7 +37,7 @@ const createInfoTemplate = (serverData) => {
 };
 
 
-export default class TripInfo {
+class TripInfo {
   constructor(data) {
     this._element = null;
     this._data = data;
@@ -62,3 +58,5 @@ export default class TripInfo {
     this._element = null;
   }
 }
+
+export default TripInfo;
