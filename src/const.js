11bcedf363401bcc74b48e45024ca848ed4401ff
Kirill <developerpopov@yandex.ru>
2020-11-28 19:08:07 +0300

    Сделал отдельный файл const

---
+++ src/const.js
@@ -0,0 +1,12 @@
+export const maxCityVisible = 3;
+
+export const HiddenHeaderList = {
+  FILTERS: `Filter events`,
+  MENU: `Switch trip view`,
+  SORT: `Trip events`
+};
+
+export const Keys = {
+  ESC: `Esc`,
+  ESCAPE: `Escape`
+};
