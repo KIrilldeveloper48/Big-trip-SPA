fb3ffb863f0dcd1598e71839b8e60408c2454e94
Kirill <developerpopov@yandex.ru>
2020-12-11 01:54:17 +0300

    Добавил логику для всех типов сортировки точек

--- src/presenter/Trip.js
+++ src/presenter/Trip.js
@@ -11,11 +11,12 @@ import NewPointView from "../view/main-new-point";
 import HiddenHeader from '../view/hidden-header';
 import Placeholder from "../view/placeholder";
 // Моковые данные
-import {SORT_LIST, FILTERS_LIST, MENU_LIST} from '../mocks/const';
+import {SORT_LIST, FILTERS_LIST, MENU_LIST, SortType} from '../mocks/const';
 import {generateTripPoints} from '../mocks/trip-point';
 // Всп. функции
 import {render, RenderPosition} from '../utils/render';
 import {updateItem} from "../utils/common";
+import {sortTime, sortPrice, sortDate} from "../utils/sorting";
 // Константы
 import {HiddenHeaderList} from '../const';
 
@@ -30,6 +31,8 @@ class Trip {
     this._pointsContainer = tripContainer.querySelector(`.trip-events`);
     // Будущий список с экземплярами точек. Ключ - айдишник конкретной точки, Свойство - экземпляр класса Point
     this._pointPresenter = {};
+    // Изначальный тип сортировки (По дате)
+    this._currentSortType = SortType.DEFAULT;
     // Компоненты для отрисовки
     this._menuComponent = new MenuView(MENU_LIST);
     this._filtersComponent = new FiltersView(FILTERS_LIST);
@@ -44,12 +47,15 @@ class Trip {
     // Привязывание контекста
     this._handlePointChange = this._handlePointChange.bind(this);
     this._handleModeChange = this._handleModeChange.bind(this);
+    this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
   }
 
   // Инициализация
-  init(pointList, sorting) {
-    // Копируем массив с данными точек, чтобы сохранить исходный порядок элементов
-    this._pointList = pointList.slice();
+  init(pointList) {
+    // Сортируем исходный массив, так как исходный порядок элементов нам нигде не требуется, везде используется уже отсортированный по датам
+    this._pointList = pointList.sort(sortDate);
+    // Отсортированный массив сохраняем, чтобы можно было восстановить сортировку по датам после других типов сортировки
+    this._sourcedPointList = this._pointList.slice();
     // Отрисовываем менюшку
     this._renderHeaderControls();
     // Отрисовываем скрытый заголовок для контентной части
@@ -60,19 +66,19 @@ class Trip {
     if (this._pointList.length === 0) {
       this._renderNoPoint();
     } else {
-      this._renderHeaderInfo(this._pointList);
+      this._renderHeaderInfo();
       this._renderSort();
-      this._renderDisplayPoint(sorting);
+      this._renderPoints();
     }
   }
 
   // Отрисовка информация для цены и маршруте путешествия
-  _renderHeaderInfo(pointList) {
+  _renderHeaderInfo() {
     const mainHeaderElement = this._tripContainer.querySelector(`.trip-main`);
-    render(mainHeaderElement, new InfoView(pointList), RenderPosition.AFTERBEGIN);
+    render(mainHeaderElement, new InfoView(this._pointList), RenderPosition.AFTERBEGIN);
 
     const tripInfoElement = mainHeaderElement.querySelector(`.trip-info`);
-    render(tripInfoElement, new CostView(pointList), RenderPosition.BEFOREEND);
+    render(tripInfoElement, new CostView(this._pointList), RenderPosition.BEFOREEND);
   }
 
   // Отрисовка меню и фильтров
@@ -88,6 +94,7 @@ class Trip {
   // Отрисовка сортировки
   _renderSort() {
     render(this._pointsContainer, this._sortComponent, RenderPosition.BEFOREEND);
+    this._sortComponent.setSortTypeChangeHandler(this._handleSortTypeChange);
   }
 
   // Отрисовка заглушки, если нет точек
@@ -113,24 +120,44 @@ class Trip {
     Object.values(this._pointPresenter).forEach((presentor) => presentor.resetView());
   }
 
+  // Метод для сортировки массива с точками, в зависимости от выбранного типа сортировки
+  _sortPoints(sortType) {
+    switch (sortType) {
+      case SortType.TIME:
+        this._pointList.sort(sortTime);
+        break;
+      case SortType.PRICE:
+        this._pointList.sort(sortPrice);
+        break;
+      default:
+        this._pointList = this._sourcedPointList.slice();
+    }
+    this._currentSortType = sortType;
+  }
+
+  // Коллбэк с логикой для отрисовки отсортированных точек
+  _handleSortTypeChange(sortType) {
+    if (this._currentSortType === sortType) {
+      return;
+    }
+    this._sortPoints(sortType);
+    this._clearPointsList();
+    this._renderPoints();
+  }
+
   // Удаление списка точек (как в объекте так и в DOM)
   _clearPointsList() {
     Object.values(this._pointPresenter).forEach((presenter) => presenter.destroy());
     this._pointPresenter = {};
   }
 
-  _renderDisplayPoint(sorting) {
-    const sortedPoint = sorting(this._pointList);
-    this._renderPoints(sortedPoint);
-  }
-
   // Отрисовка всех точек
-  _renderPoints(pointsList) {
+  _renderPoints() {
     // Отрисовка конетнера, куда будут рендериться точки
     render(this._pointsContainer, this._eventsListComponent, RenderPosition.BEFOREEND);
 
-    for (let i = 0; i < pointsList.length; i++) {
-      this._renderPoint(pointsList[i]);
+    for (let i = 0; i < this._pointList.length; i++) {
+      this._renderPoint(this._pointList[i]);
     }
   }
 
