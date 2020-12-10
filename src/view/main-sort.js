58f50f7ad3b7f7033920ab13d6359531074ddd4a
Kirill <developerpopov@yandex.ru>
2020-12-11 01:56:37 +0300

    Изменил шаблон и класс для работы сортировки

--- src/view/main-sort.js
+++ src/view/main-sort.js
@@ -6,7 +6,7 @@ const generateSortListTemplate = (sortList) => {
 
   return sortList.reduce((result, type) => {
     const typeLowerCase = type.toLowerCase();
-    const isDisabled = type === SortListDisable.EVENT || type === SortListDisable.OFFERS ? `disabled` : ``;
+    const isDisabled = type === SortListDisable.EVENT || type === SortListDisable.OFFERS ? `disabled` : `data-sort-type="${typeLowerCase}"`;
 
     result += `<div class="trip-sort__item  trip-sort__item--${typeLowerCase}">
                 <input ${isDisabled} id="sort-${typeLowerCase}" 
@@ -20,13 +20,32 @@ const generateSortListTemplate = (sortList) => {
 
 const createSortTemplate = (serverData) => {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-           ${generateSortListTemplate(serverData)}
+            ${generateSortListTemplate(serverData)}
           </form>`;
 };
 
 class Sort extends AbstractView {
+  constructor(serverData) {
+    super(serverData);
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
+
   getTemplate() {
     return createSortTemplate(this._data);
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (evt.target.tagName !== `INPUT`) {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener(`click`, this._sortTypeChangeHandler);
   }
 }
 
