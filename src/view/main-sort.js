f2a17b2c57763f14ca9770a9e2ed7e3a7003fd1f
Kirill <developerpopov@yandex.ru>
2020-12-14 14:38:29 +0300

    Поместил дата атрибут непосредственное в инпут

--- src/view/main-sort.js
+++ src/view/main-sort.js
@@ -6,11 +6,11 @@ const generateSortListTemplate = (sortList) => {
 
   return sortList.reduce((result, type) => {
     const typeLowerCase = type.toLowerCase();
-    const isDisabled = type === SortListDisable.EVENT || type === SortListDisable.OFFERS ? `disabled` : `data-sort-type="${typeLowerCase}"`;
+    const isDisabled = type === SortListDisable.EVENT || type === SortListDisable.OFFERS ? `disabled` : ``;
 
     result += `<div class="trip-sort__item  trip-sort__item--${typeLowerCase}">
                 <input ${isDisabled} id="sort-${typeLowerCase}" 
-                class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${typeLowerCase}">
+                class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${typeLowerCase}" data-sort-type="${typeLowerCase}">
                 <label class="trip-sort__btn" for="sort-${typeLowerCase}">${type}</label>
               </div>`;
     return result;
